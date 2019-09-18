2b6c487c7039d3186d50790deae23da0f5573054
Marco Stigliano <marco.stigliano.1@gmail.com>
2019-09-18 11:27:27 +0200

    Remote db configurations

--- server.js
+++ server.js
@@ -13,13 +13,12 @@ const image = require('./controllers/image');
 
 //CONSTANTS
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 const db = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
-    user: '',
-    password: '',
-    database: 'smart-brain'
+    connectString: DATABASE_URL,
+    ssl: true
   }
 });
 const app = express();
